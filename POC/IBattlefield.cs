544ea4b398e6d05a78badb8d25cb09fa9df4ed79
SinaC <heymbeeck.joel@gmail.com>
2014-06-30 11:25:29 +0200

    Started

---
+++ POC/IBattlefield.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POC
+{
+    internal interface IBattlefield
+    {
+        int Cannon(Robot robot, int degrees, int range);
+        int Drive(int degrees, int speed);
+        int Scan(int degrees, int resolution);
+    }
+}
